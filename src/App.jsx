f1aecd013452bb282130802839403ed0e8bbab5b
Priyanshu <priyanshu.sharmaiia@gmail.com>
2026-04-28 23:21:03 +0530

    Acernity UI Navbar added with framer motion and shadcn UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import NavbarDemo from './components/resizable-navbar-demo'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <NavbarDemo/>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/cars" element={<h1>Cars Page</h1>} />
+        <Route path="/bookings" element={<h1>My Bookings Page</h1>} />
+      </Routes>
+      <Footer />
+    </>
   )
 }
 
